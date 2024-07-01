ef4352df442fba96920777a358710d3907667064
mahdieh1992 <mohamadimahdieh70@gmail.com>
2024-07-01 20:20:10 +0330

    product-header

--- src/Components/Main/Products.js
+++ src/Components/Main/Products.js
@@ -2,9 +2,26 @@
 
 export default function Products(){
     return (
-        <>
-        
-        </>
+        <div className="container">
+            <svg className="hidden">
+            <symbol id="chevron-left" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+            </symbol>
+
+            </svg>
+            <div className="product-header pt-48">
+                <div>
+                    <h5 className="product-title">جدیدترین محصولات</h5>
+                    <h4 className="product-subtitle">فرآوری شده از دانه قهوه</h4>
+                </div>
+                <a className="product-link">مشاهده همه محصولات
+                    <svg className="w-5 h-5">
+                        <use href="#chevron-left"></use>
+                    </svg>
+                </a>
+            </div>
+
+        </div>
     )
 
 }
